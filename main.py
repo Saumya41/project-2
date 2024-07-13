35cdc2b0d08f51555dd1d0b9df23a686eb759d92
Saumya41 <saumyashukla4199@gmail.com>
2024-07-13 11:36:39 +0530

    webpade update

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from auth.routes import router as auth_router
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 
 @app.get("/")
-def read_root():
-    return {"message": "Welcome to the authentication service!"}
+async def read_root():
+    return {"message": "Welcome to the Password Management API"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
